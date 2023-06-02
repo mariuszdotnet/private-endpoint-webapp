2a53b181d2d31b444d6a776c65cccbfa9e6efe1c
Mariusz Kolodziej <makolo@microsoft.com>
2023-06-02 06:41:04 -0400

    are remote call

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
+import requests
 
 app = FastAPI()
 
+url = "https://error-test-mk.azurewebsites.net/api/HttpTriggerOpen?"
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    my_response = requests.get(url)
+    #print(my_response)
+    return {"message": "Hello World " + str(my_response.status_code)}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
